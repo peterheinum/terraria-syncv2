4d2dc39d6b39a4156d4939ccd58025dd456daa2d
peterheinum <peter.heinum@gmail.com>
2020-05-29 22:59:39 +0200

    29/05/2020, 22:59:39

--- src/initialize.js
+++ src/initialize.js
@@ -8,6 +8,7 @@ const { pull } = require('./pull')
 
 const start_terraria = () => opn('steam://rungameid/105600')
 const get_file_timestamp = () => fs.statSync('../' + file + '.wld')
+const go_back = () => shell.cd('terraria-syncv2')
 
 const is_new_timestamp = timestamp => {
   const { mtime } = get_file_timestamp()
@@ -22,12 +23,11 @@ const initialize = () => {
   const interval = setInterval(() => {
     if(is_new_timestamp(mtime)) {
       clearInterval(interval)
-      console.log(`end time: ${get_file_timestamp()}`)
+      console.log(`end time: ${get_file_timestamp().mtime}`)
       push().then(process.exit)
     }
   }, 5000)
 }
 
-const go_back = () => shell.cd('terraria-syncv2')
 
 pull().then(go_back).then(initialize)
